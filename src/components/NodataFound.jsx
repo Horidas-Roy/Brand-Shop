056521d175e0da09fad6b00987d81631c18ee96a
Horidas Roy <royhoridas64@gmail.com>
2023-10-21 16:12:37 +0600

    added readme.md and check responsiveness

--- src/components/NodataFound.jsx
+++ src/components/NodataFound.jsx
@@ -2,7 +2,7 @@
 
 const NodataFound = () => {
     return (
-        <div className='h-[100vh] flex justify-center items-center text-5xl font-bold'>
+        <div className='h-[100vh] w-[90vw] mx-auto flex justify-center items-center text-5xl font-bold'>
             <h2> No Data Found</h2>
         </div>
     );
--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -28,7 +28,7 @@ const MyCart = () => {
             <h2 className='text-center text-3xl font-bold py-5'>Your Cart</h2>
             {/* <h2>My Cart:{carts.length}</h2> */}
             {
-                carts?
+                carts.length>0 ?
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-5 px-4'>
                 {
                     carts.map(cart=><CartDetails 
